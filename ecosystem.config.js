925e3dcf123cef9fc4936e710ff4b002f95e0681
Jose Andres <andresjosehr@gmail.com>
2025-07-05 18:08:43 -0400

    Archivo de configuración modificado

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'project-notifications',
     script: 'start-server.js',
-    cwd: '/var/www/project-notifications',
+    cwd: '/var/www/projects/project_notifications',
     instances: 1,
     autorestart: true,
     watch: false,
@@ -11,9 +11,9 @@ module.exports = {
       NODE_ENV: 'production',
       PORT: 3000
     },
-    error_file: '/var/www/project-notifications/logs/pm2-error.log',
-    out_file: '/var/www/project-notifications/logs/pm2-out.log',
-    log_file: '/var/www/project-notifications/logs/pm2-combined.log',
+    error_file: '/var/www/projects/project_notifications/logs/pm2-error.log',
+    out_file: '/var/www/projects/project_notifications/logs/pm2-out.log',
+    log_file: '/var/www/projects/project_notifications/logs/pm2-combined.log',
     time: true
   }]
 };
